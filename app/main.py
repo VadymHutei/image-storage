c3042119f61c104dec2ca1b5d86ae90dd73606cc
Vadym Hutei <hutei@live.com>
2018-08-01 20:29:09 +0300

    [APP]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,25 @@
-from flask import Flask
-import conf
+from flask import Flask, request, redirect, render_template
+from storage import Storage
 
 app = Flask(__name__)
+
+@app.route('/acp', methods=['GET'])
+def acp():
+    return render_template('acp/main.html')
+
+@app.route('/acp/upload', methods=['GET', 'POST'])
+def upload():
+    image_urls = []
+    if request.method == 'POST':
+        if 'images' not in request.files:
+            return redirect(request.url)
+        images = request.files.getlist('images')
+        storage = Storage()
+        for image in images:
+            result = storage.save(image)
+            if result['status'] == 'success':
+                image_urls.append(result['url'])
+    return render_template('acp/upload.html', image_urls=image_urls)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
---
+++ app/storage.py
@@ -0,0 +1,10 @@
+class Storage():
+
+    def __init__(self):
+        pass
+
+    def save(self, image):
+        return {
+            'status': 'success',
+            'url': 'test'
+        }
